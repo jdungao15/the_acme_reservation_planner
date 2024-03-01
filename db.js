2bec44f4a7405a3a42e1478cd9584df3eb0bd47b
john dungao <johnmiguel.dungao@gmail.com>
2024-03-01 15:44:20 -0500

    create schemas

--- db.js
+++ db.js
@@ -1,2 +1,36 @@
-const pg = require('pg');
-const client = new pg.client(process.env.DATABASE_URL)  || 'localhost://'
+const pg = require("pg");
+const client =
+  new pg.client(process.env.DATABASE_URL) || "localhost://restaurant_db";
+
+const init = async () => {
+  await client.connect();
+  console.log("Connected to db");
+
+  const SQL = `
+        DROP TABLE IF EXISTS customers;
+        DROP TABLE IF EXISTS restaurants;
+        DROP TABLE IF EXISTS reservations;
+
+        CREATE TABLE customers(
+            id UUID PRIMARY KEY,
+            name VARCHAR(100)
+        );
+
+        CREATE TABLE restaurants(
+            id UUID PRIMARY KEY,
+            name VARCHAR(100)
+        );
+
+        CREATE TABLE reservations(
+            id UUID PRIMARY KEY,
+            date DATE NOT NULL,
+            party_count INTEGER NOT NULL,
+            restaurant UUID REFERENCES restaurants(id) NOT NULL,
+            customer_id UUID REFERENCES customers(id) NOT NULL
+        );
+    `;
+  await client.query(SQL);
+  console.log("database seeded");
+};
+
+init();
