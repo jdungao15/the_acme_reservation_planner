e71fea95f5390f649b27f1a22c99e7e203d41426
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-01 17:23:37 -0500

    finish unit 34

--- db.js
+++ db.js
@@ -1,14 +1,78 @@
 const pg = require("pg");
-const client =
-  new pg.client(process.env.DATABASE_URL) || "localhost://restaurant_db";
+const client = new pg.Client(process.env.DATABASE_URL) || "localhost://restaurant_db";
+const uuid = require('uuid')
+
+
+const createCustomer = async (name) => {
+    const SQL = `
+        INSERT INTO customers(id,name) VALUES($1,$2)
+        RETURNING *
+    
+    `
+    const res = await client.query(SQL, [uuid.v4(), name])
+    return res.rows[0];
+}
+
+const createRestaurant = async (name) => {
+    const SQL = `
+        INSERT INTO restaurants(id,name) VALUES($1,$2)
+        RETURNING *
+    `
+    const res = await client.query(SQL, [uuid.v4(), name]);
+    return res.rows[0];
+}
+
+const fetchCustomers = async () => {
+    const SQL = `
+        SELECT * FROM customers
+    `
+    const res = await client.query(SQL);
+    return res.rows;
+}
+
+const fetchReservations = async () => {
+    const SQL = `
+        SELECT * FROM reservations
+    `
+    const res = await client.query(SQL)
+    return res.rows;
+}
+
+const fetchRestaurants = async () => {
+    const SQL = `
+        SELECT * FROM restaurants
+    `
+    const res = await client.query(SQL);
+    return res.rows;
+}
+
+const createReservation = async ({date, party_count, restaurant_id, customer_id}) => {
+    const SQL = `
+        INSERT INTO reservations(id, date, party_count, restaurant_id, customer_id) VALUES($1,$2,$3,$4,$5)
+        RETURNING *
+    
+    `
+    const response = await client.query(SQL, [uuid.v4(), date, party_count, restaurant_id, customer_id]);
+    return response.rows;
+}
+
+const destroyReservation = async ({reservationId, customer_id}) => {
+    const SQL = `
+        DELETE FROM reservations WHERE id=$1 AND customer_id = $2
+    
+    `
+    const response = await client.query(SQL,[reservationId, customer_id]);
+    return response.rows
+
+}
+
 
 const createTable = async () => {
-
-
   const SQL = `
+        DROP TABLE IF EXISTS reservations;
         DROP TABLE IF EXISTS customers;
         DROP TABLE IF EXISTS restaurants;
-        DROP TABLE IF EXISTS reservations;
+        
 
         CREATE TABLE customers(
             id UUID PRIMARY KEY,
@@ -24,7 +88,7 @@ const createTable = async () => {
             id UUID PRIMARY KEY,
             date DATE NOT NULL,
             party_count INTEGER NOT NULL,
-            restaurant UUID REFERENCES restaurants(id) NOT NULL,
+            restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
             customer_id UUID REFERENCES customers(id) NOT NULL
         );
     `;
@@ -33,4 +97,4 @@ const createTable = async () => {
 };
 
 
-modules.exports = {client, createTable}
+module.exports = {client, fetchReservations, createTable, createCustomer, createRestaurant, createReservation, fetchCustomers, fetchRestaurants,destroyReservation}
--- index.js
+++ index.js
@@ -1,11 +1,65 @@
 const express = require('express');
 const app = express();
-const {createTable,client } = require('./db.js')
-const PORT = process.env.PORT || 3000;
+const {client, createTable, createCustomer, createRestaurant, createReservation, fetchCustomers, fetchRestaurants,destroyReservation, fetchReservations} = require('./db.js')
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 
+
+app.get('/api/customers', async(req,res,next) => {
+    try {
+        res.send(await fetchCustomers());
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get('/api/restaurants', async(req,res,next) => {
+    try {
+        res.send(await fetchRestaurants());
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get('/api/reservations', async(req,res,next) => {
+    try {
+        res.send(await fetchReservations());
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.post('/api/customers/:id/reservations', async (req,res,next) => {
+    const {date, party_count, restaurant_id} = req.body;
+    const customer_reservations = {
+        date,
+        party_count,
+        restaurant_id,
+        customer_id: req.params.id
+    }
+
+   
+    try {
+        const response = await createReservation(customer_reservations);
+        res.send(response)
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.delete('/api/customers/:customer_id/reservations/:id', async (req, res) => {
+    const customer_id = req.params.customer_id;
+    const reservationId = req.params.id;
+
+    try {
+        const result = await destroyReservation({customer_id, reservationId});
+        res.sendStatus(204);
+    } catch (error) {
+        next(error);
+    }
+});
 
 
 const init = async() => {
@@ -13,6 +67,52 @@ const init = async() => {
     console.log('Connected to DB....')
 
     await createTable();
+    const [john, maiden, matthew, redlobster,cast,jayhan]  = await Promise.all([
+        createCustomer('John'), 
+        createCustomer('Maiden'), 
+        createCustomer('Matthew'), 
+        createRestaurant('Red Lobster'), 
+        createRestaurant('Cast Iron Pot'),
+        createRestaurant('Jayhan\s Grill'),
+    ]);
+
+    const reservation1 = {
+        date: '2023-05-13',
+        party_count: 10,
+        restaurant_id: redlobster.id,
+        customer_id: john.id,
+
+    }
+       const reservation2 = {
+        date: '2022-10-21',
+        party_count: 10,
+        restaurant_id: redlobster.id,
+        customer_id: john.id,
+
+    }
+       const reservation3 = {
+        date: '2024-11-05',
+        party_count: 10,
+        restaurant_id: redlobster.id,
+        customer_id: john.id,
+
+    }
+       const reservation4 = {
+        date: '2023-10-05',
+        party_count: 10,
+        restaurant_id: redlobster.id,
+        customer_id: john.id,
+
+    }
+
+    // console.log(customers)
+    const reservations = await Promise.all([
+        createReservation(reservation1),
+        createReservation(reservation2),
+        createReservation(reservation3),
+        createReservation(reservation4)
+
+    ])
 
     app.listen(PORT, () => {
         console.log(`Server listening on PORT${PORT}`)
